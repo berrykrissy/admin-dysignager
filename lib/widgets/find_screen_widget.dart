04d1451056a715e4d36d88565cdbfcac386e122a
Benedict Contawe <benedictcontawe@gmail.com>
2023-05-20 00:35:37 +0800

    Update Find Screen Widget, Still On going for Maps

--- lib/widgets/find_screen_widget.dart
+++ lib/widgets/find_screen_widget.dart
@@ -8,11 +8,10 @@ class FindScreenWidget extends BaseWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
+    return Flexible (
       flex: 70,
       child: Container (
         padding: const EdgeInsets.all(30),
-        color: Colors.yellow,
         alignment: Alignment.center,
         child: Column(
           children: [
@@ -23,21 +22,24 @@ class FindScreenWidget extends BaseWidget {
                 Text("Dysignager", style: TextStyle ( fontSize: 25, fontWeight: FontWeight.w500,),),
               ],
             ),
+            const SizedBox(height: 13,),
             Card (
-              color: Colors.transparent,
+              color: Colors.white,
               shape: RoundedRectangleBorder(
                 side: const BorderSide(
                   color: Colors.grey,
                 ),
                 borderRadius: BorderRadius.circular(10.0),
               ),
-              child: const Row(
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+                child: const Row (
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Column(
                     children: [
                       Icon (
-                        CupertinoIcons.map_pin_slash,
+                        CupertinoIcons.map_pin_ellipse,
                         color: Colors.green,
                       ),
                       Text("Online", style: TextStyle ( fontSize: 13, fontWeight: FontWeight.w500,),),
@@ -47,7 +49,7 @@ class FindScreenWidget extends BaseWidget {
                   Column(
                     children: [
                       Icon (
-                        CupertinoIcons.map_pin_slash,
+                        CupertinoIcons.map_pin_ellipse,
                         color: Colors.red,
                       ),
                       Text("Out of Sync", style: TextStyle ( fontSize: 13, fontWeight: FontWeight.w500,),),
@@ -57,7 +59,7 @@ class FindScreenWidget extends BaseWidget {
                   Column(
                     children: [
                       Icon (
-                        CupertinoIcons.map_pin_slash,
+                        CupertinoIcons.map_pin_ellipse,
                         color: Colors.orange,
                       ),
                       Text("Offline", style: TextStyle ( fontSize: 13, fontWeight: FontWeight.w500,),),
@@ -67,7 +69,7 @@ class FindScreenWidget extends BaseWidget {
                   Column(
                     children: [
                       Icon (
-                        CupertinoIcons.map_pin_slash,
+                        CupertinoIcons.map_pin_ellipse,
                         color: Colors.blue,
                       ),
                       Text("Disabled", style: TextStyle ( fontSize: 13, fontWeight: FontWeight.w500,),),
@@ -76,6 +78,18 @@ class FindScreenWidget extends BaseWidget {
                   Text("0", style: TextStyle ( fontSize: 25, fontWeight: FontWeight.w500,),),
                 ],
               ),
+              )
+            ),
+            const SizedBox(height: 13,),
+            const Card (
+              child: SizedBox (
+                width: double.infinity,
+                height: 250,
+                child: Icon (
+                  CupertinoIcons.map_fill,
+                  color: Colors.blue,
+                ),
+              )
             ),
           ]
         ),
