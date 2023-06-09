b09feea79eeae241551c8f7ea9a5f33066d4d7aa
Benedict Contawe <benedictcontawe@gmail.com>
2023-06-10 01:15:19 +0800

    update location model

--- lib/models/locations_model.dart
+++ lib/models/locations_model.dart
@@ -7,9 +7,21 @@ class LocationsModel {
   String? onlineSince;
   String? status;
 
-  LocationsModel({this.name, this.address});
+  LocationsModel( {
+    this.name, 
+    this.address,
+    this.gps, 
+    this.onlineSince,
+    this.status,
+  } );
 
-  Map<String, dynamic> toMap() => {"name": name, "address": address};
+  Map<String, dynamic> toMap() => {
+    "name": name, 
+    "address": address,
+    "gps": gps,
+    "online_since": onlineSince,
+    "status": status,
+  };
 
   LocationsModel.fromSnapshot(DocumentSnapshot snapshot) {
     final data = snapshot.data() as Map<String,dynamic>;
