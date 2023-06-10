e05e22ff458b6702fcdf224f06dfad04d07ca06a
Benedict Contawe <benedictcontawe@gmail.com>
2023-06-11 01:57:03 +0800

    Add Observable Contents

--- lib/widgets/data_table_contents_widget.dart
+++ lib/widgets/data_table_contents_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:signage/controllers/dashboard_controller.dart';
 import 'package:signage/widgets/base_widgets.dart';
 
@@ -8,22 +9,28 @@ class DataTableContentsWidget extends BaseWidget<DashboardController> {
 
   @override
   Widget build(BuildContext context) {
-    return DataTable (
-      columns: const <DataColumn> [
-        DataColumn(label: Text("Client", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
-        DataColumn(label: Text("Start Date", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
-        DataColumn(label: Text("End Date", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
-        DataColumn(label: Text("Duration", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
-        DataColumn(label: Text("File Name", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
-      ],
-      rows : controller.getContentsDetails().map(
-        (cell) => DataRow(cells: <DataCell> [
-            DataCell(Text(cell.client ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
-            DataCell(Text(cell.startDate ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
-            DataCell(Text(cell.endDate ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),    
-            DataCell(Text(cell.duration ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),    
-            DataCell(Text(cell.fileName ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),    
-        ] ) ).toList(),
-    );
+    return Obx(() {
+      if (controller.isLoading.value) {
+        return const Center(child: CircularProgressIndicator());
+      } else {
+        return DataTable (
+          columns: const <DataColumn> [
+            DataColumn(label: Text("Client", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
+            DataColumn(label: Text("Start Date", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
+            DataColumn(label: Text("End Date", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
+            DataColumn(label: Text("Duration", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
+            DataColumn(label: Text("File Name", style: TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
+          ],
+          rows : controller.getContentsDetails().map(
+            (cell) => DataRow(cells: <DataCell> [
+                DataCell(Text(cell.client ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
+                DataCell(Text(cell.startDate ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),
+                DataCell(Text(cell.endDate ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),    
+                DataCell(Text(cell.duration ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),    
+                DataCell(Text(cell.fileName ?? "", style: const TextStyle ( color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500,),),),    
+            ] ) ).toList(),
+        );
+      }
+    } );
   }
 }
