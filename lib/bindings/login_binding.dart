8b25dfed6b6cffdf1498a21c6a614bc717d2c563
Benedict Contawe <benedictcontawe@gmail.com>
2023-06-13 00:59:23 +0800

    Add GPS jutsu

--- lib/bindings/login_binding.dart
+++ lib/bindings/login_binding.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:signage/bindings/base_binding.dart';
 import 'package:signage/controllers/login_controller.dart';
+import 'package:signage/services/firebase/firestore_service.dart';
 
 class LoginBinding extends BaseBinding {
 
@@ -10,7 +11,7 @@ class LoginBinding extends BaseBinding {
     debugPrint("LoginBinding dependencies");
     Get.lazyPut<LoginController> ( 
       () => LoginController (
-
+        Get.find<FirestoreService>(),
       )
     );
   }
--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,24 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:signage/controllers/base_controller.dart';
+import 'package:signage/models/locations_model.dart';
 import 'package:signage/routes/app_pages.dart';
+import 'package:signage/services/firebase/firestore_service.dart';
+import 'package:signage/utils/constants.dart';
 
 class LoginController extends BaseController {
 
-  LoginController() {
+  LoginController(FirestoreService this._service,) {
     debugPrint("LoginController Constructor");
   }
 
+final FirestoreService _service;
+LocationsModel? _locations = null;
   RxBool isLoading = false.obs;
   final TextEditingController? usernameController = new TextEditingController();
   final TextEditingController? passwordController = new TextEditingController();
 
   @override
-  void onInit() {
+  Future<void> onInit() async {
     super.onInit();
     debugPrint("LoginController _launchLogin");
     _handleLocationPermission();
+    getLocation();
   }
 
   Future<bool> _handleLocationPermission() async {
@@ -45,9 +52,29 @@ class LoginController extends BaseController {
     return true;
   }
 
+  Future<void> getLocation() async {
+    debugPrint("SplashController getLocation");
+    final snapshot = await _service.getLocationsByIdByCity(Constants.CITY);
+    _locations = snapshot.firstOrNull;
+  }
+
   Future<void> checkCredentials() async {
     debugPrint("LoginController checkCredentials ${usernameController?.text} ${passwordController?.text}");
     isLoading(true);
+    if (await _handleLocationPermission()) {
+      Position position = await Geolocator.getCurrentPosition ( desiredAccuracy: LocationAccuracy.best);
+      _service.updateLocation (
+        _locations?.id, 
+        LocationsModel (
+          name: _locations?.name,
+          address: _locations?.address,
+          gps: GeoPoint(position.latitude ,position.longitude),
+          onlineSince: _locations?.onlineSince,
+          status: _locations?.status,
+          isEnabled: _locations?.isEnabled,
+        ).toMap()
+      );
+    }
     if(usernameController?.text == "Admin" && passwordController?.text == "123") {
       Get.snackbar("Credentials", "Login Succeed");
       _launchDashboard();
